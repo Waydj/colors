bae62d859f8d74860b1f06d226ebc7d57d055d12
waydj <farhminait@gmail.com>
2024-02-21 20:14:15 +0200

    add react-toastify

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Navbar } from "./Navbar";
 import { ColorBox } from "./ColorBox";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Palette.css";
 
 export const Palette = ({ palette }) => {
@@ -9,6 +11,7 @@ export const Palette = ({ palette }) => {
 
   const changeFormat = (format) => {
     setFormat(format.value);
+    toast.success("Format changed to " + format.value);
   };
 
   const colorBoxes = palette.colors[level].map((color) => (
@@ -19,6 +22,11 @@ export const Palette = ({ palette }) => {
     <div className="palette">
       <Navbar level={level} setLevel={setLevel} changeFormat={changeFormat} />
       <div className="palette-colors">{colorBoxes}</div>
+      <ToastContainer
+        autoClose={3000}
+        theme="colored"
+        position="bottom-center"
+      />
     </div>
   );
 };
