dada0476b903547e2389930cf4c8bf361bf5f74b
waydj <farhminait@gmail.com>
2024-02-26 22:52:51 +0200

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { Route, Routes, useParams } from "react-router-dom";
+import { PaletteList } from "./PaletteList";
 import { Palette } from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
+const RouteComponent = () => {
+  const params = useParams();
+
+  const findPalette = (id) => {
+    return seedColors.find((palette) => palette.id === id);
+  };
+
+  return <Palette palette={generatePalette(findPalette(params.id))} />;
+};
+
 const App = () => {
-  return <Palette palette={generatePalette(seedColors[4])} />;
+  return (
+    <Routes>
+      <Route path="/" element={<PaletteList />} />
+      <Route path="/palette/:id" element={<RouteComponent />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/PaletteList.jsx
@@ -0,0 +1,3 @@
+export const PaletteList = () => {
+  return <div>PaletteList</div>;
+};
