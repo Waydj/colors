181284e35f488b52ea4d554d5a8c173803ae8587
waydj <farhminait@gmail.com>
2024-03-24 13:08:42 +0200

    styled DraggableColorBox

--- src/DraggableColorBox.jsx
+++ src/DraggableColorBox.jsx
@@ -1,18 +1,45 @@
+import styled from "@emotion/styled";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const StyledBox = styled.div`
+  background-color: ${(props) => props.color};
+  height: 25%;
+  width: 20%;
+  margin: 0 auto;
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  margin-bottom: -3.5px;
+  &:hover svg {
+    color: white;
+    transform: scale(1.5);
+  }
+`;
+
+const StyledBoxContent = styled.div`
+  position: absolute;
+  width: 100%;
+  padding: 10px;
+  left: 0;
+  bottom: 0;
+  color: black;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-size: 12px;
+  display: flex;
+  justify-content: space-between;
+`;
+
 export const DraggableColorBox = ({ color, name }) => {
   return (
-    <div
-      style={{
-        backgroundColor: color,
-        height: "25%",
-        width: "20%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-3.5px",
-      }}
-    >
-      {name}
-    </div>
+    <StyledBox color={color}>
+      <StyledBoxContent>
+        <span>{name}</span>
+        <DeleteIcon
+          color="error"
+          style={{ transition: "all 0.3s ease-in-out" }}
+        />
+      </StyledBoxContent>
+    </StyledBox>
   );
 };
